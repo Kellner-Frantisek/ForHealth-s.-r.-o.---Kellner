7bae00d09eef62e6f33fe648cfbcff4b56d542f3
login-form-kellner <kellnerfrantisek66@gmail.com>
2025-11-10 21:50:04 +0100

    about-us page + main page

--- includes/footer.php
+++ includes/footer.php
@@ -1,21 +1,4 @@
-        <div class="footer" id="contact">
-            <div class="footer__box">
-
-                <div class="footer__box__contact">
-                    <div class="footer__box__contact__text">
-                        email: pindour@ekodom.cz <br>
-                        tel. č.: 6767676767<br>
-                    </div>
-                </div>
-
-                <div class="footer__box__button">
-                    <a href="contact.php" class="footer__box__button__main"> 
-                        Kontaktní formulář
-                    </a>
-                </div>
-
-            </div>
-
+        <div class="footer">
             <div class="footer__h1">
                 <div class="footer__h1__text">
                     © 2025 ForHealth, s. r. o.
--- onas.php
+++ onas.php
@@ -4,33 +4,33 @@ include "includes/menu.php"
 
 <div class="about-us">
     <div class="about-us__photo"></div>
-    <div class="about-us__text">My jsme firma ForHealth, s. r. o.</div>
+    <div class="about-us__text">My jsme ForHealth, s. r. o.</div>
 
     <div class="about-us__people">
         <div class="about-us__people__box">
-            <div class="about-us__people__box__photo"></div>
+            <div class="about-us__people__box__photo"><img src="images/ja.jpg" alt=""></div>
             <div class="about-us__people__box__text">František Kellner</div>
             <div class="about-us__people__box__pozice">pozice</div>
         </div>
         <div class="about-us__people__box">
-            <div class="about-us__people__box__photo"></div>
+            <div class="about-us__people__box__photo"><img src="images/honekjpg.jpg" alt=""></div>
             <div class="about-us__people__box__text">Honza Kupeček</div>
             <div class="about-us__people__box__pozice">pozice</div>
         </div>
         <div class="about-us__people__box">
-            <div class="about-us__people__box__photo"></div>
+            <div class="about-us__people__box__photo"><img src="images/fifan.jpg" alt=""></div>
             <div class="about-us__people__box__text">Filip Procházka</div>
             <div class="about-us__people__box__pozice">pozice</div>
         </div>
         <div class="about-us__people__box">
-            <div class="about-us__people__box__photo"></div>
+            <div class="about-us__people__box__photo"><img src="images/bem.jpg" alt=""></div>
             <div class="about-us__people__box__text">Jakub Böhm</div>
             <div class="about-us__people__box__pozice">pozice</div>
         </div>
         <div class="about-us__people__box">
-            <div class="about-us__people__box__photo"></div>
+            <div class="about-us__people__box__photo"><img src="images/hepner.jpg" alt=""></div>
             <div class="about-us__people__box__text">Tomáš Hepner</div>
-            <div class="about-us__people__box__pozice">pozice</div>
+            <div class="about-us__people__box__pozice">zib zib</div>
         </div>
         <div class="about-us__people__box">
             <div class="about-us__people__box__photo"><img src="images/halacka.jpg" alt=""></div>
@@ -38,7 +38,7 @@ include "includes/menu.php"
             <div class="about-us__people__box__pozice">pozice</div>
         </div>
         <div class="about-us__people__box">
-            <div class="about-us__people__box__photo"></div>
+            <div class="about-us__people__box__photo"><img src="images/halacka.jpg" alt=""></div>
             <div class="about-us__people__box__text">Krystýna Pechová</div>
             <div class="about-us__people__box__pozice">pozice</div>
         </div>
